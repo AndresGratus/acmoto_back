dff5b2dfa1de69b3b5cb46ae3011b50f9dc8fbbc
Andres Castañeda <andres.castaneda280040@gmail.com>
2024-11-06 09:07:05 -0500

    Initial commit

---
+++ src/main/java/com/acmoto_back/AcmotoBackApplication.java
@@ -0,0 +1,13 @@
+package com.acmoto_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmotoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcmotoBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acmoto_back/AcmotoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acmoto_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmotoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
